1036ea911dad58693840d47a7e4c0b7221a8a33b
imsidz <imsidzluv@gmail.com>
2017-07-17 16:46:53 +0530

    edit mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'sid@agcialg.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'email@email.com'),
         'name' => env('MAIL_FROM_NAME', 'Admin'),
     ],
 
